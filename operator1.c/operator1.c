0075565a64b4b04d05ddc3e88b55c67c9412940f
Prasanth Arul <amprasanth05@gmail.com>
2023-10-06 20:40:07 +0530

    operators

---
+++ operator1.c/operator1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a,b;
+    a=2,b=3;
+    printf("%d\nthe sum:",(a+b));
+    printf("%d\nsubtraction:",(a-b));
+    printf("%d\nproduct:",(a*b));
+    printf("%d\ndivide:",(a/b));
+    printf("%d\npercentage:",(a%b));
+    return 0;
+    
+}
