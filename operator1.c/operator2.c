972e7736a98260a9da84fb29b482cd44f75ffbf4
Prasanth Arul <amprasanth05@gmail.com>
2023-10-06 20:53:01 +0530

    operators

---
+++ operator1.c/operator2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int n,m;
+    printf("Enter the value of positive integers is n:");
+    scanf("%d",&n);
+    m=n%5;
+    printf("The value of last digit m:%d",m);
+    return 0;
+    
+}
